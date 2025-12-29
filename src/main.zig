6214b086fbe71b494605b96b9e58453b898e7795
egit90 <elie90@gmail.com>
2025-12-29 13:47:40 -0500

    init

---
+++ src/main.zig
@@ -0,0 +1,35 @@
+const std = @import("std");
+
+pub const Value = union(enum) {
+    null_value,
+    bool_value: bool,
+    number: f64,
+    string: []const u8,
+    array: []const Value,
+};
+
+test "create string value" {
+    const str = Value{ .string = "hello" };
+    try std.testing.expect(std.mem.eql(u8, str.string, "hello"));
+}
+
+test "create number value" {
+    const int_val = Value{ .number = 42 };
+    const float_val = Value{ .number = 42.14 };
+    try std.testing.expect(int_val.number == 42.0);
+    try std.testing.expect(float_val.number == 42.14);
+}
+
+test "create null value" {
+    const val = Value{ .null_value = {} };
+    try std.testing.expect(val == .null_value);
+}
+
+test "create boolean value" {
+    const val_true = Value{ .bool_value = true };
+    const val_false = Value{ .bool_value = false };
+    try std.testing.expect(val_true.bool_value == true);
+    try std.testing.expect(val_false.bool_value == false);
+}
+
+pub fn main() !void {}
